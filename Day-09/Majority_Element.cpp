49b4328e75f3417659fa607bc8444774d3db0c47
Sameerjain_084 <Jainsameer179@gmail.com>
2026-02-01 19:53:19 +0530

    Day 09: Majority Element using Boyer–Moore Voting Algorithm

---
+++ Day-09/Majority_Element.cpp
@@ -0,0 +1,55 @@
+/*
+========================================
+Problem: Majority Element (LC #169)
+========================================
+
+Problem Statement:
+Given an array nums of size n, return the majority element.
+The majority element is the element that appears more than ⌊n / 2⌋ times.
+You may assume that the majority element always exists in the array.
+
+Approach Used:
+Boyer–Moore Voting Algorithm
+- Maintain a candidate element and a frequency counter.
+- If the frequency becomes 0, choose the current element as the new candidate.
+- Increment the frequency if the current element matches the candidate,
+  otherwise decrement it.
+- The final candidate will be the majority element.
+
+Complexity Analysis:
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+Why This Approach?
+Since the majority element appears more than n/2 times, it will always remain
+as the final candidate after canceling out all other elements using the
+Boyer–Moore Voting Algorithm.
+
+Concepts Used:
+Arrays, Greedy Strategy, Voting Algorithm
+
+Problem Link:
+https://leetcode.com/problems/majority-element/
+========================================
+*/
+
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int freq=0,ans=0;
+        for(int i=0;i<n;i++) {
+            if (freq==0) {
+                ans=nums[i];
+            }
+            if(ans==nums[i]) {
+                freq++;
+            } else {
+                freq--;
+            }
+        }
+       return ans;
+
+    }
+};
